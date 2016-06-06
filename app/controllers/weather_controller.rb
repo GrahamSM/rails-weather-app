97ec6965719a75583c3f13bb886577775c1fa36a
graham <graham.sm94@gmail.com>
2016-06-06 09:09:03 -0400

    Updated city naming

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,16 +4,12 @@ class WeatherController < ApplicationController
     @weather = Weather.new
   end
 
-  def index
-
-  end
-
   def show
     @weather = Weather.find(params[:id])
   end
 
   def create
-    @weather = Weather.new(city: params[:current_observation][:observation_location][:city], lat: params[:current_observation][:observation_location][:latitude], lon: params[:current_observation][:observation_location][:longitude], temp_c: params[:current_observation][:temp_c], temp_f: params[:current_observation][:temp_f], relative_humidity: params[:current_observation][:relative_humidity],
+    @weather = Weather.new(city: params[:current_observation][:display_location][:full], lat: params[:current_observation][:observation_location][:latitude], lon: params[:current_observation][:observation_location][:longitude], temp_c: params[:current_observation][:temp_c], temp_f: params[:current_observation][:temp_f], relative_humidity: params[:current_observation][:relative_humidity],
     wind_string: params[:current_observation][:wind_string], temperature_string: params[:current_observation][:temperature_string], observation_time: params[:current_observation][:observation_time])
     respond_to do |format|
       if @weather.save
@@ -34,10 +30,6 @@ class WeatherController < ApplicationController
         format.json {render json: @weather, location: @weather}
     end
   end
-
-  def update
-  end
-
 
 
 end
